7160ac05b869960e723afd1e0c0a4c749bcb37d4
baturovm <azizbek.boturow@gmail.com>
2021-04-05 13:21:09 +0700

    Create SharedViewModel.kt

---
+++ SharedViewModel.kt
@@ -0,0 +1,25 @@
+package dev.lostfilm.client.application
+
+import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dev.lostfilm.client.ui.extension.navigation.NavResult
+
+class SharedViewModel : ViewModel() {
+
+    private val result: MutableLiveData<NavResult?> = MutableLiveData()
+
+    fun setResult(key: String, bundle: Bundle) {
+        result.value = NavResult(key, bundle)
+    }
+
+    fun clearResult() {
+        result.value = null
+    }
+
+    fun getResultLiveData() : LiveData<NavResult?> {
+        return result
+    }
+
+}
